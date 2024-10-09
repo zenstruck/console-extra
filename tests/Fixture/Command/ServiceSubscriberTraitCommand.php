86e9b5905b0b36df0f7945ce79648dfb61bf06fd
Kevin Bond <kevinbond@gmail.com>
2024-10-08 21:56:29 -0400

    minor: add BC layer for `ServiceSubscriberTrait`

--- tests/Fixture/Command/ServiceSubscriberTraitCommand.php
+++ tests/Fixture/Command/ServiceSubscriberTraitCommand.php
@@ -1,45 +1,32 @@
 <?php
 
-/*
- * This file is part of the zenstruck/console-extra package.
- *
- * (c) Kevin Bond <kevinbond@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Zenstruck\Console\Tests\Fixture\Command;
 
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Service\Attribute\SubscribedService;
+use Symfony\Contracts\Service\ServiceMethodsSubscriberTrait;
 use Symfony\Contracts\Service\ServiceSubscriberTrait;
-use Zenstruck\Console\InvokableServiceCommand;
-use Zenstruck\Console\IO;
 
-/**
- * @author Kevin Bond <kevinbond@gmail.com>
- */
-final class ServiceSubscriberTraitCommand extends InvokableServiceCommand
-{
-    use ServiceSubscriberTrait;
-
-    public function __invoke(IO $io, RouterInterface $router): void
+if (trait_exists(ServiceMethodsSubscriberTrait::class)) {
+    final class ServiceSubscriberTraitCommand extends BaseServiceSubscriberTraitCommand
     {
-        $io->comment(\sprintf('IO: %s', \get_debug_type($io)));
-        $io->comment(\sprintf('RouterInterface: %s', \get_debug_type($router)));
-        $io->comment(\sprintf('LoggerInterface: %s', \get_debug_type($this->logger())));
+        use ServiceMethodsSubscriberTrait;
+
+        #[SubscribedService]
+        protected function logger(): LoggerInterface
+        {
+            return $this->container->get(__METHOD__);
+        }
     }
-
-    public static function getDefaultName(): string
+} else {
+    final class ServiceSubscriberTraitCommand extends BaseServiceSubscriberTraitCommand
     {
-        return 'service-subscriber-trait-command';
-    }
+        use ServiceSubscriberTrait;
 
-    #[SubscribedService]
-    private function logger(): LoggerInterface
-    {
-        return $this->container->get(__METHOD__);
+        #[SubscribedService]
+        protected function logger(): LoggerInterface
+        {
+            return $this->container->get(__METHOD__);
+        }
     }
 }
